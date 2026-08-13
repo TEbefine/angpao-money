446768dc24695bfe3c8ffa636d95a3514667216c
Teera1995 <teera@frontware.co.th>
2026-08-13 15:01:19 +0700

    feat: update feed UI with swipe-to-act gestures and refine interaction handling

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='rap-v2';
+const V='rap-v6';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(V).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));
