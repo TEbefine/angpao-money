ea0509fb57a7edc5243ae810474c2b0fadd28913
Teera1995 <teera@frontware.co.th>
2026-08-13 17:22:24 +0700

    feat: implement auto-calculation for transfer amounts and improve shop closing summary UI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='rap-v21';
+const V='rap-v23';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(V).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));
