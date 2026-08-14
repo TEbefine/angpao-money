c006dfe9d1643f3041858bb96583d172c01fbd04
Teera1995 <teera@frontware.co.th>
2026-08-14 14:55:28 +0700

    feat: add logo to lock screen and increment service worker cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='rap-v27';
+const V='rap-v28';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon.webp','./apple-touch-icon.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(V).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));
